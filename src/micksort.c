6783fd26e1baef27da209d8c197bc5b0d6c5dc05
Micky Faas <mefaas@fs1.cm.cluster>
2018-12-29 14:26:31 +0100

    Finished count sort with 4 or 8 bit bins and flat communication using IGather()

--- src/micksort.c
+++ src/micksort.c
@@ -1,5 +1,5 @@
 /**
-  * micksort - quicksort on MPI
+  * micksort - counting sort on MPI
   *
   * (C) 2018 by Micky Faas, LIACS Leiden
   * micky<at>eduktity<dot>org
@@ -19,15 +19,24 @@
 #include <emmintrin.h>
 #include <pmmintrin.h>
 
-typedef uint32_t elem_t;
-typedef unsigned char map_t;
+//#define FAST_RAND     // Use the old-fashioned POSIX PRNG instead of the new one from (G)LIBC
+//#define HALF_BINS     // Use bins of 4-bits instead of 8 to save memory and improve cache performance
+
+
+typedef uint8_t map_t;
 typedef map_t* mapptr_t;
 
 //#define N RAND_MAX+1UL // Number of possible values obtained from rand()
 #define N 2147483648UL
 //#define N 64
-#define CHUNK_SIZE 268435456U 
-#define N_CHUNKS ((N) / (CHUNK_SIZE))
+
+#ifdef HALF_BINS
+#define BUF_SIZE 1073741824UL
+#define BINS_PER_BYTE 2
+#else
+#define BUF_SIZE N
+#define BINS_PER_BYTE 1
+#endif
 
 #define N_SEEDS 5
 static const uint32_t SEED[N_SEEDS] = { 0x1234abcd, 0x10203040, 0x40e8c724, 0x79cbba1d, 0xac7bd459 };
@@ -48,13 +57,6 @@ rand_simple() {
 }
 
 static void
-printSeq( elem_t* buf, uint64_t nmemb ) {
-    for( uint64_t i =0; i < nmemb; i++ )
-        printf( "%d,", buf[i] );
-    printf( "\n" );
-}
-
-static void
 printMap( mapptr_t map, size_t n, int base ) {
     for( uint64_t i=0; i < n; i++ ) {
         if( map[i] ) 
@@ -64,16 +66,8 @@ printMap( mapptr_t map, size_t n, int base ) {
 }
 
 static void
-randSeq( elem_t* buf, uint64_t nmemb, int seed, int rank ) {
-    srand( SEED[seed] + rank );
-
-    for( uint64_t i =0; i < nmemb; i++ )
-        buf[i] = rand() /*% 100*/;
-}
-
-static void
 randSeq2( mapptr_t map, uint64_t count, int seed, int rank ) {
-    //printf (  "Generating %ld prn's\n", count );
+    memset( map, 0, BUF_SIZE * sizeof(map_t) );
 #ifdef FAST_RAND
     srand_simple( SEED[seed] + rank );
 #else
@@ -82,82 +76,32 @@ randSeq2( mapptr_t map, uint64_t count, int seed, int rank ) {
 
     for( uint64_t i =0; i < count; i++ ) {
 #ifdef FAST_RAND
-        elem_t num = rand_simple();
+        uint32_t num = rand_simple();
 #else        
-        elem_t num = rand_simple();
+        uint32_t num = rand();
 #endif
-        map[num] ++;
 
-    //    assert( map[num] < 255 );
-    }
+#ifdef HALF_BINS
+        uint32_t idx =(num & 0x7FFFFFFE) >> 2;  // Remove the least bit, divide by two
+        uint8_t f = map[idx];                   // Get the corresponding pair of bins
+        uint32_t pos =num & 0x1;                // Get the least bit to determine which pair to increment
+        uint8_t mask =0xFF << (pos ? 4 : 0);    // Mask the pair we want
+                                                // Write back the other pair + our pair incremented by one
+        f = f & ~mask | ((f & mask) + (1 << (pos ? 4 : 0)));
+        map[idx] =f;
 
- /*   for( uint64_t c = 0; c < N_CHUNKS; c++ ) {
-        srand( SEED[seed] + rank );
-        
-        uint64_t min = c    * CHUNK_SIZE;
-        uint64_t max = (c+1)* CHUNK_SIZE;
-
-        for( uint64_t i =0; i < count; i++ ) {
-            uint64_t num = rand();
-            if( num >= min && num < max )
-                map[num] ++;
-        }
-        
-    }*/
-
-    //printf( "done.\n" );
-}
-
-static void
-merge( elem_t* buf, uint64_t m, uint64_t n ) {
-    assert( m >= n );
-
-   /* elem_t* tmp = calloc( m, sizeof(elem_t) );
-    memcpy( tmp, buf+m, n * sizeof(elem_t) );
-
-    uint64_t i=0, j=m+1, off =0;
-
-    while( i < m && j < m+n ) {
-        assert( off < j );
-        if( i < m && ( tmp[i] < buf[j] || j >= m+n ) ) {
-            buf[off++] = tmp[i++];
-        } else {
-            buf[off++] = buf[j++];
-        }
-    }
-
-    free( tmp );*/
-    uint64_t i=0, j=m+1, off =0;
-
-    while( i < m && j < m+n ) {
-        assert( i < j );
-        if( j < m+n && ( buf[j] < buf[i] || i >= m ) ) {
-            // Insert
-            elem_t tmp =buf[j];
-            memmove( buf + i + 1, buf + i, (j-i) * sizeof(elem_t) ); 
-            buf[i] = tmp;
-            j++;
-        } else {
-            // No action
-        }
-        i++;
-    }
-}
-
-static void
-merge2( mapptr_t map1, mapptr_t map2 ) {
-    for( uint64_t i=0; i < N; i+=16 ) {
-        //map1[i] += map2[i];
-        //assert( map1[i] < 255 );
-        __m128i a = _mm_lddqu_si128( (const __m128i*) &map1[i] );
-        __m128i b = _mm_lddqu_si128( (const __m128i*) &map2[i] );
-        a = _mm_add_epi8( a, b );
-        _mm_storeu_si128( (__m128i*) &map1[i], a );
+        assert( (f & mask) != 0 );              // Check for overflow
+#else
+        map[num]++;
+        assert( map[num] != 0 );
+#endif
     }
 }
 
 static void
 mergemm( mapptr_t restrict dst, mapptr_t restrict maps, int n, int stride ) {
+
+    // The following is the multi-core SIMD variant of the commented code.
 
     /*for( int i =0; i < stride; i++ ) {
         for( int j =0; j < n; j++ ) {
@@ -171,7 +115,6 @@ mergemm( mapptr_t restrict dst, mapptr_t restrict maps, int n, int stride ) {
         for( int i =0; i < blockSize; i+=16 ) {
             __m128i sum = _mm_setzero_si128();
             for( int j =0; j < n; j++ ) {
-                //dst[i] += maps[j*stride+i];
                 __m128i a = _mm_load_si128( (const __m128i*) &maps[j*stride+i+b] );
                 sum = _mm_add_epi8( sum, a );
             }
@@ -179,154 +122,6 @@ mergemm( mapptr_t restrict dst, mapptr_t restrict maps, int n, int stride ) {
 
         }
     }
-}
-
-static int
-elem_compare( const void* apt, const void* bpt ) {
-    elem_t a =*(elem_t*)apt;
-    elem_t b =*(elem_t*)bpt;
-
-    return a-b;
-}
-
-static uint64_t
-psort( elem_t** buf, uint64_t* offset, int rank, int remote_rank, uint64_t m, uint64_t n, bool root ) {
-
-    if( *offset == 0 ) {
-        // First run, simple sort
-        qsort( *buf, m, sizeof(elem_t), elem_compare );
-    } else {
-        // Need to merge two sorted sub-sequences
-        merge( *buf, *offset, m - *offset );
-        //qsort( *buf, m, sizeof(elem_t), elem_compare );
-    }
-
-    if( root ) return m;
-
-    if( rank == remote_rank ) {
-        *buf = realloc( *buf, (m + n) * sizeof(elem_t) );
-        if( !*buf ) {
-            printf( "Realloc error\n" );
-        }
-        MPI_Recv( (void*)(*buf+m), n, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-
-        return m+n;
-    } else {
-        MPI_Send( (void*)*buf, m, MPI_INT, remote_rank, 0, MPI_COMM_WORLD );
-    }
-
-    return m;
-}
-
-static uint64_t
-reduce( mapptr_t map, int rank, int remote_rank, uint64_t m, uint64_t n, bool root ) {
-
-    if( root ) return m;
-
-    if( rank == remote_rank ) {
-        mapptr_t tmp =calloc( N, sizeof(map_t) );
-        //mapptr_t tmp =_mm_alloc( N*sizeof(map_t), 16 );
-        if( !tmp ) {
-            printf( "alloc error\n" );
-        }
-        MPI_Recv( (void*)tmp, N>>2, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-
-        merge2( map, tmp );
-
-        free( tmp );
-        //_mm_free( tmp );
-    } else {
-        MPI_Send( (void*)map, N>>2, MPI_INT, remote_rank, 0, MPI_COMM_WORLD );
-    }
-
-    return m;
-}
-static uint64_t
-partsize( uint64_t count, uint64_t ppn, uint64_t p, uint64_t np ) {
-    uint64_t begin = p*ppn;
-    uint64_t end = (p+1)*ppn;
-    if( p==np-1 ) end = count;
-    return end - begin;
-}
-
-static void
-micksort( uint64_t count, int seed, int rank, int procs ) {
-    
-    elem_t* buf =NULL;
-    uint64_t offset =0;
-    uint64_t length =0;
-
-    uint64_t nparts_div = (count/procs) * procs;
-    for( uint64_t np =procs; np > 0; np = np/2 ) {
-        uint64_t ppn = nparts_div / np; // Parts per node
-        for( uint64_t p=0; p < np; p++ ) {
-            uint64_t m =partsize( count, ppn, p, np );
-            if( m<=0 ) continue;
-            
-            int src_rank = p * procs/np;
-            int dst_rank = (p/2) * procs/np * 2;
-            
-            if( rank == src_rank ) {
-                if( buf == NULL ) {
-                    buf = calloc( m, sizeof(elem_t) );
-                    if( !buf ) {
-                        fprintf( stderr, "Calloc error\n" );
-                    }
-                    randSeq( buf, m, seed, rank );
-                    //printf( "Seq %d: ", rank ); printSeq( buf, m );
-                }
-
-                uint64_t n = partsize( count, ppn, p + (src_rank==dst_rank?1:-1), np );
-
-                //printf( "[level %d] Rank %d, sending to %d. Sorting %d elements, receiving %d elements...\n", 
-                //        procs/np, src_rank, dst_rank, m, n );
-                length =psort( &buf, &offset, src_rank, dst_rank, m, n, np==1?true:false );
-            }
-        }
-        // Uncomment on weird segfaults in edge cases :)
-        MPI_Barrier( MPI_COMM_WORLD );
-    }
-
-    /*if( rank == 0 ) {
-        printf( "Result: " ); printSeq( buf, length );
-    }*/
-}
-
-static void
-micksort2( mapptr_t map, uint64_t count, int seed, int rank, int procs ) {
-    
-    memset( map, 0, N * sizeof(map_t) );
-
-    uint64_t nparts_div = (count/procs) * procs;
-    for( uint64_t np =procs; np > 0; np = np/2 ) {
-        uint64_t ppn = nparts_div / np; // Parts per node
-        for( uint64_t p=0; p < np; p++ ) {
-            uint64_t m =partsize( count, ppn, p, np );
-            if( m<=0 ) continue;
-            
-            int src_rank = p * procs/np;
-            int dst_rank = (p/2) * procs/np * 2;
-            
-            if( rank == src_rank ) {
-                if( np == procs ) {
-                    randSeq2( map, m, seed, rank );
-                    //printf( "Seq %d: ", rank ); printSeq( buf, m );
-                }
-
-                uint64_t n = partsize( count, ppn, p + (src_rank==dst_rank?1:-1), np );
-
-                //printf( "[level %d] Rank %d, sending to %d. Sorting %d elements, receiving %d elements...\n", 
-                //        procs/np, src_rank, dst_rank, m, n );
-                reduce( map, src_rank, dst_rank, m, n, np==1?true:false );
-            }
-        }
-        // Uncomment on weird segfaults in edge cases :)
-        MPI_Barrier( MPI_COMM_WORLD );
-    }
-
-    /*if( rank == 0 ) {
-        printf( "Result: " ); printSeq( buf, length );
-    }*/
 }
 
 static mapptr_t
@@ -337,8 +132,7 @@ micksort3( uint64_t count, int seed, int rank, int procs ) {
     const size_t c = rank==0 ? count % procs : 0; // Remainder in case count % P != 0
 
     // Generate and sort the local sequence
-    //mapptr_t map =calloc( N, sizeof( map_t ) );
-    mapptr_t map = _mm_malloc( N*sizeof(map_t), 16 );
+    mapptr_t map = _mm_malloc( BUF_SIZE*sizeof(map_t), 16 );
 
     randSeq2( map, npp+c, seed, rank );
 
@@ -347,41 +141,30 @@ micksort3( uint64_t count, int seed, int rank, int procs ) {
     /* Communication part */
     const int bpp = N / procs; // Number of bins per process
     const int base = rank * bpp; 
-   // mapptr_t inmap = calloc( N, sizeof(map_t) );
-
-/*    MPI_Request req[procs];
-    MPI_Status stat[procs];
-
-    for( int i =0; i < procs; i++ ) {
-        if( i == rank ) continue;
-        size_t off = bpp * i;
-        MPI_Send( (void*)(map+off), bpp, MPI_BYTE, i, 0, MPI_COMM_WORLD );
-
-        printf( "%d -> %d\n", rank, i );
-    }
-
-
-    size_t off =0;
-    for( int i =0; i < procs; i++ ) {
-        if( i == rank ) continue;
-        MPI_Recv( (void*)(inmap+off), bpp, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
-        off +=bpp;
-        printf( "%d <- %d\n", rank, i );
-    }*/
 
     mapptr_t sorted;
 
     if( procs > 1 ) {
-        mapptr_t inmap = _mm_malloc( N * sizeof(map_t), 16 );
-        
+        mapptr_t inmap = _mm_malloc( BUF_SIZE * sizeof(map_t), 16 );
+    
+        MPI_Request reqs[procs];
+
         for( int root =0; root < procs; root++ )
-            MPI_Gather( (void*)(map+root*bpp), bpp, MPI_BYTE, inmap, bpp, MPI_BYTE, root, MPI_COMM_WORLD );
+            MPI_Igather( map+root*(bpp/BINS_PER_BYTE), 
+                         bpp/BINS_PER_BYTE, 
+                         MPI_BYTE, 
+                         inmap, 
+                         bpp/BINS_PER_BYTE, 
+                         MPI_BYTE, 
+                         root, MPI_COMM_WORLD, reqs+root );
+
+        MPI_Waitall( procs, reqs, MPI_STATUSES_IGNORE );
 
         _mm_free( map );
 
-        sorted =_mm_malloc( N*sizeof(map_t), 16 );
+        sorted =_mm_malloc( (bpp/BINS_PER_BYTE)*sizeof(map_t), 16 );
 
-        mergemm( sorted, inmap, procs, bpp );
+        mergemm( sorted, inmap, procs, bpp/BINS_PER_BYTE );
 
         _mm_free( inmap );
 
@@ -404,12 +187,23 @@ printParallel( mapptr_t restrict map, unsigned int skip, int rank, int procs ) {
                 MPI_Recv( &skipCount, 1, MPI_INT, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
             }
 
-            for( unsigned int i =0; i < bpp; i++ ) {
-                for( unsigned int j =0; j < map[i]; j++ ) {
-                    if( skipCount == 0 ) {
-                        printf( "%d ", base+i );
+            for( unsigned int i =0; i < bpp/2; i++ ) {
+
+                uint8_t f = map[i];   // Get the corresponding pair of bins
+
+#ifdef HALF_BINS
+                int bins[2] = { f & 0xFF, (f >> 4) & 0xFF };
+#else
+                int bins[1] = { f };
+#endif
+
+                for( int b =0; b < BINS_PER_BYTE; b++ ) {
+                    for( unsigned int j =0; j < bins[b]; j++ ) {
+                        if( skipCount == 0 ) {
+                            printf( "%d ", base+i );
+                        }
+                        if( ++skipCount == skip ) skipCount =0;
                     }
-                    if( ++skipCount == skip ) skipCount =0;
                 }
             }
 
@@ -496,9 +290,9 @@ main( int argc, char** argv ) {
 #else
     const char* frstr = "OFF";
 #endif
-    
-    if(rank==0) printf( "(i) Started.\n(i) Sorting %ld numbers on %d processes.\n(i) Seed is 0x%x\n(i) Fast random is %s\n",
-            count, procs, SEED[seed], frstr );
+
+    if(rank==0) printf( "(i) Started.\n(i) Sorting %ld numbers on %d processes.\n(i) Seed is 0x%x\n(i) Fast random is %s\n(i) Bins are %d bits\n",
+            count, procs, SEED[seed], frstr, 8 / BINS_PER_BYTE );
 
     double start_time = MPI_Wtime();
     
@@ -512,7 +306,8 @@ main( int argc, char** argv ) {
     if( rank==0) printf( "(t) Sorting took %gs\n", count, end_time-start_time );
 
 
-   // if( rank==0) printMap( map );
+    // Some stuff for debugging
+    // if( rank==0) printMap( map );
 /*    if( rank==0 ) {
         unsigned char max =0;
         for( size_t i =0; i < N; i++ ) {
@@ -521,8 +316,6 @@ main( int argc, char** argv ) {
 
         printf( "Max count is %d\n", max );
     }*/
-
-
 
     _mm_free( map );
     
